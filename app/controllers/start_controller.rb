58794463939eb54cb0722bf166f1e07abfad723c
Henri Knafo <cmoi40@live.ca>
2013-09-08 19:59:25 -0400

    lll9

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,7 +1,10 @@
 class StartController < ApplicationController
   def index
     @result = request.location    
+   
     @mytimezone = NearestTimeZone.to(@result.latitude,@result.longitude)
+ 
+    
     @timenowuser = DateTime.now.in_time_zone(@mytimezone)
   end
 end
