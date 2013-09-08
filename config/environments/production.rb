80b5a723bfbb3d3c205c21cc3b5da37fc53be39b
Henri Knafo <cmoi40@live.ca>
2013-09-08 17:21:40 -0400

    fixing precompile issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,11 @@ Woo::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.action_mailer.default_url_options = { :host => "www.yourhost.com" }
+  config.action_mailer.default_url_options = { :host => "http://woomatch.com" }
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
