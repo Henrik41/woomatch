48eabeab5fd6f6b7f66f770772bcf4f31a77c3cf
Henri Knafo <cmoi40@live.ca>
2013-11-18 20:56:59 -0500

    trying stuff2

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,6 +1,7 @@
+  require 'activeadmin'
 ActiveAdmin.setup do |config|
 
-  require 'activeadmin'
+
   config.site_title = "Woo"
   config.authentication_method = :authenticate_admin_user!
   config.current_user_method = :current_admin_user
