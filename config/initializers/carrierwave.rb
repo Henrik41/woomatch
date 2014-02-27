52524947cb8d6c96abd682577b7d9e47f249fbd9
Henri Knafo <cmoi40@live.ca>
2014-02-27 08:27:35 -0500

    lst commit feb2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,13 @@
-AWS_CONFIG = YAML.load_file("#{::Rails.root}/config/aws.yml")[::Rails.env]
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = AWS_CONFIG['bucket']
+  config.aws_bucket = ENV['S3_BUCKET_NAME']
   config.aws_acl    = :public_read
 #  config.asset_host = 'http://localhost:3000'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
-    access_key_id:     AWS_CONFIG['access_key_id'],
-    secret_access_key: AWS_CONFIG['secret_access_key']
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     
      }
 end
