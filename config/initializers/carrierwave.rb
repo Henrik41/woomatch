3e60e5a71ac921ed1a4af09a9cf18283eceb4c97
Henri Knafo <cmoi40@live.ca>
2014-03-01 19:44:20 -0500

    march 1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV['S3_BUCKET_NAME']
-  config.aws_acl    = :public_read
+  config.aws_acl    = false
 #  config.asset_host = 'http://localhost:3000'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
