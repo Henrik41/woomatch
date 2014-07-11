cf61ebdf3b5cd93205d495c9d4a1e63a32379d4c
Henri Knafo <cmoi40@live.ca>
2014-07-11 10:30:58 -0400

    commit2 july 11 2014

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,6 @@
 //= require jquery.modal
 //= require image-picker
 //= require gritter
-//= require ion.rangeSlider.min
 //= require jquery.validationEngine-zh_CN
 //= require jquery.validationEngine-en.js
 //= require jquery.validationEngine
