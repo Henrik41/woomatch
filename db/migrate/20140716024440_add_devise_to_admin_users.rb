0807b2a42af56d87b71e6d248b0251e282e35538
Henri Knafo <cmoi40@live.ca>
2014-07-16 09:01:09 -0400

    commit4 july 15 2014

--- db/migrate/20140716024440_add_devise_to_admin_users.rb
+++ db/migrate/20140716024440_add_devise_to_admin_users.rb
@@ -1,9 +1,9 @@
 class AddDeviseToAdminUsers < ActiveRecord::Migration
   def self.up
-    change_table(:admin_users) do |t|
+    create_table(:admin_users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "admin@woomatch.com"
+      t.string :encrypted_password, null: false, default: "qqqqqqqw"
 
       ## Recoverable
       t.string   :reset_password_token
