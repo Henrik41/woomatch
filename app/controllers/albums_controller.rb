ade018f4b06b9c3d772f7b5ba57f1154c2985d85
Henri Knafo <cmoi40@live.ca>
2014-11-24 18:10:14 -0500

    adding album - final

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -81,7 +81,7 @@ class AlbumsController < ApplicationController
     @album.destroy
 
     respond_to do |format|
-      format.html { redirect_to albums_url }
+      format.html { redirect_to '/profile/edit' }
       format.json { head :no_content }
     end
   end
