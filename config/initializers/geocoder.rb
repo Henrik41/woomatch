cace17ce823ac625deb0546a18248126aa78c190
Henri Knafo <cmoi40@live.ca>
2017-01-09 21:43:25 -0500

    adding sidkiq

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,6 +3,7 @@ Geocoder.configure(
    # :lookup => :bing, :api_key => 'Astd_ygIwPyTMpRFhEeosP6n4RlScOEvj1BBXuYG3wxnOdDavbq8-DEu5CBnFknw',
   :lookup => :google,
   :ip_lookup => :ipinfo_io,
+  :freegeoip  => {:host => "localhost:8080"},
   :cache => Rails.cache, 
  
 )
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,3 +8,9 @@ stdout_path "#{root}/log/unicorn-stdout.log"
 listen "/tmp/unicorn.woomatch.sock"
 worker_processes 2
 timeout 60
+
+after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+end
