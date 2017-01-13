95ccd647b51f91791831ddf9d6dbd995d2bbd077
Henri Knafo <cmoi40@live.ca>
2017-01-12 22:29:56 -0500

    geocoder changed

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,7 +2,7 @@ Geocoder.configure(
  :timeout => 20,
    # :lookup => :bing, :api_key => 'Astd_ygIwPyTMpRFhEeosP6n4RlScOEvj1BBXuYG3wxnOdDavbq8-DEu5CBnFknw',
   :lookup => :google,
-  :ip_lookup => :ipinfo_io,
+  :ip_lookup => :freegeoip,
   :freegeoip  => {:host => "localhost:8080"},
   :cache => Rails.cache, 
  
