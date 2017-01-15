8307a8a8b512c92ed1a54c26989aa1893177bbbe
Henri Knafo <cmoi40@live.ca>
2017-01-14 23:32:50 -0500

    delete cache

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,6 @@
 Geocoder.configure(
  :timeout => 20,
-   # :lookup => :bing, :api_key => 'Astd_ygIwPyTMpRFhEeosP6n4RlScOEvj1BBXuYG3wxnOdDavbq8-DEu5CBnFknw',
-  :lookup => :google,
+  :lookup => :bing, :api_key => 'Astd_ygIwPyTMpRFhEeosP6n4RlScOEvj1BBXuYG3wxnOdDavbq8-DEu5CBnFknw',
   :ip_lookup => :freegeoip,
   :freegeoip  => {:host => "localhost:8080"},
   :cache => Rails.cache, 
